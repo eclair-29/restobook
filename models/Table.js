4a7252df4d288cb8e9326ba4529b7e02a18fd540
Migz De Chavez <dmgzky@gmail.com>
2018-12-28 17:25:46 +0800

    12.28.18 - document table's model

---
+++ models/Table.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongoosePaginate = require('mongoose-paginate');
+
+const TableSchema = new Schema({
+    tableName: { type: String, required: true, unique: true },
+    seatCapacity: { type: Number, required: true, max: 999 },
+    reservations: [
+        { type: Schema.Types.ObjectId, ref: 'reservation' }
+    ],
+    reservationCount: { type: Number, default: 0, max: 9999 },
+    dateAdded: { type: Date, default: Date.now }
+});
+
+TableSchema.plugin(mongoosePaginate);
+
+const Table = mongoose.model('table', TableSchema);
+
+module.exports = Table;
